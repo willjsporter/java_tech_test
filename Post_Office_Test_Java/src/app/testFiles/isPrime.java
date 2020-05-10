30817129e1f217e87e0b7434fb2d48b39b5ffabe
Will <willjsporter@hotmail.co.uk>
2020-05-10 22:10:13 +0100

    feat(isPrime): ensure prime numbers return true
    tests already in place to ensure non-primes return false

--- Post_Office_Test_Java/src/app/testFiles/isPrime.java
+++ Post_Office_Test_Java/src/app/testFiles/isPrime.java
@@ -1,8 +1,17 @@
 package app.testFiles;
 
+import java.util.stream.IntStream;
+
 public class isPrime {
 	public static Boolean isNumberPrime(int number) {
-		//Implement your code here
-		return false;
+		if(number < 2) {
+			return false;
+		} else {
+			int maximumPossibleSmallerFactor = new Double(Math.sqrt(number)).intValue();
+			return IntStream
+				.rangeClosed(2,maximumPossibleSmallerFactor)
+				.filter(possibleFactor -> number % possibleFactor == 0)
+				.count() == 0;
+		}
 	}
 }
--- Post_Office_Test_Java/test/app/testFiles/isPrimeTest.java
+++ Post_Office_Test_Java/test/app/testFiles/isPrimeTest.java
@@ -17,4 +17,17 @@ public class isPrimeTest {
     public void shouldReturnFalseFor1() {
         assertFalse(isNumberPrime(1));
     }
+
+    @Test
+    public void shouldReturnFalseFor0() {
+        assertFalse(isNumberPrime(0));
+    }
+
+    @Test
+    public void shouldReturnTrueForPrimeNumbers() {
+        assertTrue(isNumberPrime(2));
+        assertTrue(isNumberPrime(17));
+        assertTrue(isNumberPrime(37));
+        assertTrue(isNumberPrime(233));
+    }
 }
